3cfebc0877ffa0febadc57cd3ad5c9373f96b83f
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-10-14 14:06:11 -0400

    All REST handlers consider OpenTracing span context.

--- atlas.com/drg/monster/drop/resource.go
+++ atlas.com/drg/monster/drop/resource.go
@@ -3,15 +3,45 @@ package drop
 import (
 	"atlas-drg/drop"
 	"atlas-drg/json"
+	"atlas-drg/rest"
 	"github.com/gorilla/mux"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
 
+const (
+	getDropById = "get_drop_by_id"
+)
+
 func InitResource(router *mux.Router, l logrus.FieldLogger) {
 	sRouter := router.PathPrefix("/drops/{id}").Subrouter()
-	sRouter.HandleFunc("", HandleGetDropById(l))
+	sRouter.HandleFunc("", registerGetDropById(l))
+}
+
+type idHandler func(id uint32) http.HandlerFunc
+
+func parseId(l logrus.FieldLogger, next idHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		value, err := strconv.ParseUint(vars["id"], 10, 32)
+		if err != nil {
+			l.WithError(err).Errorf("Error parsing id as integer")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		next(uint32(value))(w, r)
+	}
+}
+
+func registerGetDropById(l logrus.FieldLogger) http.HandlerFunc {
+	fl := l.WithFields(logrus.Fields{"originator": "GetDropById", "type": "rest_handler"})
+	return rest.RetrieveSpan(getDropById, func(span opentracing.Span) http.HandlerFunc {
+		return parseId(fl, func(id uint32) http.HandlerFunc {
+			return handleGetDropById(fl)(span)(id)
+		})
+	})
 }
 
 // GenericError is a generic error message returned by a server
@@ -19,55 +49,44 @@ type GenericError struct {
 	Message string `json:"message"`
 }
 
-func HandleGetDropById(fl logrus.FieldLogger) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		l := fl.WithFields(logrus.Fields{"originator": "GetDropById", "type": "rest_handler"})
-		dropId := getDropId(l)(r)
-		d, err := GetDropById(dropId)
-		if err != nil {
-			rw.WriteHeader(http.StatusNotFound)
-			json.ToJSON(&GenericError{Message: err.Error()}, rw)
-			return
-		}
+func handleGetDropById(fl logrus.FieldLogger) func(span opentracing.Span) func(dropId uint32) http.HandlerFunc {
+	return func(span opentracing.Span) func(dropId uint32) http.HandlerFunc {
+		return func(dropId uint32) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				d, err := GetDropById(dropId)
+				if err != nil {
+					w.WriteHeader(http.StatusNotFound)
+					json.ToJSON(&GenericError{Message: err.Error()}, w)
+					return
+				}
 
-		rw.WriteHeader(http.StatusOK)
-		result := drop.DropDataContainer{Data:
-		drop.DropData{
-			Id:   strconv.Itoa(int(d.Id())),
-			Type: "com.atlas.drg.rest.attribute.DropAttributes",
-			Attributes: drop.DropAttributes{
-				WorldId:         d.WorldId(),
-				ChannelId:       d.ChannelId(),
-				MapId:           d.MapId(),
-				ItemId:          d.ItemId(),
-				EquipmentId:     d.EquipmentId(),
-				Quantity:        d.Quantity(),
-				Meso:            d.Meso(),
-				DropType:        d.Type(),
-				DropX:           d.X(),
-				DropY:           d.Y(),
-				OwnerId:         d.OwnerId(),
-				OwnerPartyId:    d.OwnerPartyId(),
-				DropTime:        d.DropTime(),
-				DropperUniqueId: d.DropperId(),
-				DropperX:        d.DropperX(),
-				DropperY:        d.DropperY(),
-				CharacterDrop:   d.CharacterDrop(),
-				Mod:             d.Mod(),
-			},
-		}}
-		json.ToJSON(result, rw)
-	}
-}
-
-func getDropId(l logrus.FieldLogger) func(r *http.Request) uint32 {
-	return func(r *http.Request) uint32 {
-		vars := mux.Vars(r)
-		value, err := strconv.ParseUint(vars["id"], 10, 32)
-		if err != nil {
-			l.WithError(err).Errorf("Error parsing id as integer")
-			return 0
+				w.WriteHeader(http.StatusOK)
+				result := drop.DropDataContainer{Data: drop.DropData{
+					Id:   strconv.Itoa(int(d.Id())),
+					Type: "com.atlas.drg.rest.attribute.DropAttributes",
+					Attributes: drop.DropAttributes{
+						WorldId:         d.WorldId(),
+						ChannelId:       d.ChannelId(),
+						MapId:           d.MapId(),
+						ItemId:          d.ItemId(),
+						EquipmentId:     d.EquipmentId(),
+						Quantity:        d.Quantity(),
+						Meso:            d.Meso(),
+						DropType:        d.Type(),
+						DropX:           d.X(),
+						DropY:           d.Y(),
+						OwnerId:         d.OwnerId(),
+						OwnerPartyId:    d.OwnerPartyId(),
+						DropTime:        d.DropTime(),
+						DropperUniqueId: d.DropperId(),
+						DropperX:        d.DropperX(),
+						DropperY:        d.DropperY(),
+						CharacterDrop:   d.CharacterDrop(),
+						Mod:             d.Mod(),
+					},
+				}}
+				json.ToJSON(result, w)
+			}
 		}
-		return uint32(value)
 	}
 }
--- atlas.com/drg/world/resource.go
+++ atlas.com/drg/world/resource.go
@@ -4,15 +4,59 @@ import (
 	drop2 "atlas-drg/drop"
 	"atlas-drg/json"
 	"atlas-drg/monster/drop"
+	"atlas-drg/rest"
 	"github.com/gorilla/mux"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
 
+const (
+	getDropsInMap = "get_drops_in_map"
+)
+
 func InitResource(router *mux.Router, l logrus.FieldLogger) {
 	iRouter := router.PathPrefix("/worlds/{worldId}/channels/{channelId}/maps/{mapId}/drops").Subrouter()
-	iRouter.HandleFunc("", GetDropsInMap(l))
+	iRouter.HandleFunc("", registerGetDropsInMap(l))
+}
+
+type mapHandler func(worldId byte, channelId byte, mapId uint32) http.HandlerFunc
+
+func parseMap(l logrus.FieldLogger, next mapHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		worldId, err := strconv.ParseUint(vars["worldId"], 10, 8)
+		if err != nil {
+			l.WithError(err).Errorf("Error parsing worldId as byte")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		channelId, err := strconv.ParseUint(vars["channelId"], 10, 8)
+		if err != nil {
+			l.WithError(err).Errorf("Error parsing channelId as byte")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		mapId, err := strconv.ParseUint(vars["mapId"], 10, 32)
+		if err != nil {
+			l.WithError(err).Errorf("Error parsing mapId as uint32")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		next(byte(worldId), byte(channelId), uint32(mapId))(w, r)
+	}
+}
+
+func registerGetDropsInMap(l logrus.FieldLogger) http.HandlerFunc {
+	return rest.RetrieveSpan(getDropsInMap, func(span opentracing.Span) http.HandlerFunc {
+		fl := l.WithFields(logrus.Fields{"originator": "GetDropById", "type": "rest_handler"})
+		return parseMap(fl, func(worldId byte, channelId byte, mapId uint32) http.HandlerFunc {
+			return handleGetDropsInMap(fl)(span)(worldId, channelId, mapId)
+		})
+	})
 }
 
 // GenericError is a generic error message returned by a server
@@ -20,81 +64,49 @@ type GenericError struct {
 	Message string `json:"message"`
 }
 
-func GetDropsInMap(fl logrus.FieldLogger) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		l := fl.WithFields(logrus.Fields{"originator": "GetDropById", "type": "rest_handler"})
-		wid := getWorldId(l)(r)
-		cid := getChannelId(l)(r)
-		mid := getMapId(l)(r)
+func handleGetDropsInMap(l logrus.FieldLogger) func(span opentracing.Span) func(worldId byte, channelId byte, mapId uint32) http.HandlerFunc {
+	return func(span opentracing.Span) func(worldId byte, channelId byte, mapId uint32) http.HandlerFunc {
+		return func(worldId byte, channelId byte, mapId uint32) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				ds, _ := drop.GetDropsForMap(worldId, channelId, mapId)
 
-		ds, _ := drop.GetDropsForMap(wid, cid, mid)
-		rw.WriteHeader(http.StatusOK)
-		result := drop2.DropDataListContainer{}
-		result.Data = make([]drop2.DropData, 0)
+				result := drop2.DropDataListContainer{}
+				result.Data = make([]drop2.DropData, 0)
 
-		for _, d := range ds {
-			data := drop2.DropData{
-				Id:   strconv.Itoa(int(d.Id())),
-				Type: "com.atlas.drg.rest.attribute.DropAttributes",
-				Attributes: drop2.DropAttributes{
-					WorldId:         d.WorldId(),
-					ChannelId:       d.ChannelId(),
-					MapId:           d.MapId(),
-					ItemId:          d.ItemId(),
-					EquipmentId:     d.EquipmentId(),
-					Quantity:        d.Quantity(),
-					Meso:            d.Meso(),
-					DropType:        d.Type(),
-					DropX:           d.X(),
-					DropY:           d.Y(),
-					OwnerId:         d.OwnerId(),
-					OwnerPartyId:    d.OwnerPartyId(),
-					DropTime:        d.DropTime(),
-					DropperUniqueId: d.DropperId(),
-					DropperX:        d.DropperX(),
-					DropperY:        d.DropperY(),
-					CharacterDrop:   d.CharacterDrop(),
-					Mod:             d.Mod(),
-				},
+				for _, d := range ds {
+					data := drop2.DropData{
+						Id:   strconv.Itoa(int(d.Id())),
+						Type: "com.atlas.drg.rest.attribute.DropAttributes",
+						Attributes: drop2.DropAttributes{
+							WorldId:         d.WorldId(),
+							ChannelId:       d.ChannelId(),
+							MapId:           d.MapId(),
+							ItemId:          d.ItemId(),
+							EquipmentId:     d.EquipmentId(),
+							Quantity:        d.Quantity(),
+							Meso:            d.Meso(),
+							DropType:        d.Type(),
+							DropX:           d.X(),
+							DropY:           d.Y(),
+							OwnerId:         d.OwnerId(),
+							OwnerPartyId:    d.OwnerPartyId(),
+							DropTime:        d.DropTime(),
+							DropperUniqueId: d.DropperId(),
+							DropperX:        d.DropperX(),
+							DropperY:        d.DropperY(),
+							CharacterDrop:   d.CharacterDrop(),
+							Mod:             d.Mod(),
+						},
+					}
+					result.Data = append(result.Data, data)
+				}
+
+				w.WriteHeader(http.StatusOK)
+				err := json.ToJSON(result, w)
+				if err != nil {
+					l.WithError(err).Errorf("Uanble to encode result.")
+				}
 			}
-			result.Data = append(result.Data, data)
 		}
-		json.ToJSON(result, rw)
-	}
-}
-
-func getWorldId(l logrus.FieldLogger) func(r *http.Request) byte {
-	return func(r *http.Request) byte {
-		vars := mux.Vars(r)
-		value, err := strconv.ParseUint(vars["worldId"], 10, 8)
-		if err != nil {
-			l.WithError(err).Errorf("Error parsing worldId as byte")
-			return 0
-		}
-		return byte(value)
-	}
-}
-
-func getChannelId(l logrus.FieldLogger) func(r *http.Request) byte {
-	return func(r *http.Request) byte {
-		vars := mux.Vars(r)
-		value, err := strconv.ParseUint(vars["channelId"], 10, 8)
-		if err != nil {
-			l.WithError(err).Errorf("Error parsing channelId as byte")
-			return 0
-		}
-		return byte(value)
-	}
-}
-
-func getMapId(l logrus.FieldLogger) func(r *http.Request) uint32 {
-	return func(r *http.Request) uint32 {
-		vars := mux.Vars(r)
-		value, err := strconv.ParseUint(vars["mapId"], 10, 32)
-		if err != nil {
-			l.WithError(err).Errorf("Error parsing mapId as uint32")
-			return 0
-		}
-		return uint32(value)
 	}
 }
